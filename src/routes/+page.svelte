56efe7c328422d962906dcccf26cfe8ae4d47e72
NoobChicken2 <lucaklaus921@gmail.com>
2024-12-30 18:53:46 +0100

    Test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<main class="h-[100vh] flex justify-center items-center bg-pink-200"> 
-    <div class="w-[90vw] h-[50vh] flex flex-col justify-center align-center item-center text-center">
+<main class="h-[100vh] flex justify-center items-center bg-blue-200"> 
+    <div>
+        <p class="text-[100px] font-bold">🚧Under construction🚧</p>
+    </div>
+    <!-- <div class="w-[90vw] h-[50vh] flex flex-col justify-center align-center item-center text-center">
         <p>Irin zemer te dua shume 💖💖💖</p>
         <p>Ta dish qe te kam shume xhan</p>
         <p>dhe dua te te perqafoj shume fort <br> dhe te puth deri sa te te merret fryma <br> ne keto momente</p>
@@ -8,5 +11,5 @@
         <img src="https://media.tenor.com/WIXakzgi4KkAAAAM/sorry-bae.gif" 
              alt="GiveYouMyHeart"
              class="w-52 h-52 self-center">
-    </div>
+    </div> -->
 </main>
