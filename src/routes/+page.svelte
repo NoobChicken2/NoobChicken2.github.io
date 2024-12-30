cb8cae8c0b16fa100daebe24b078244acd72dcf2
Klaus Kazazi <lucaklaus921@gmail.com>
2024-12-30 18:44:04 +0100

    Test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="h-[100vh] flex justify-center items-center bg-pink-200"> 
+    <div class="w-[90vw] h-[50vh] flex flex-col justify-center align-center item-center text-center">
+        <p>Irin zemer te dua shume 💖💖💖</p>
+        <p>Ta dish qe te kam shume xhan</p>
+        <p>dhe dua te te perqafoj shume fort <br> dhe te puth deri sa te te merret fryma <br> ne keto momente</p>
+        <p>😘😘😘😘😘😘😘</p>
+        <br>
+        <img src="https://media.tenor.com/WIXakzgi4KkAAAAM/sorry-bae.gif" 
+             alt="GiveYouMyHeart"
+             class="w-52 h-52 self-center">
+    </div>
+</main>
